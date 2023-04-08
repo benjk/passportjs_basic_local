36a58c3eead517a0dad5f2ffe2e77d34bd0da9c1
Benjam <benjamin.kielinski@hotmail.fr>
2023-04-08 03:43:34 +0200

    clean session / bdd

--- models/database.js
+++ models/database.js
@@ -1,43 +1,33 @@
 const Sequelize = require("sequelize");
 const DataTypes = require('sequelize/lib/data-types');
 
-/**
- * -------------- DATABASE ----------------
- */
-
+// Constantes à exporter en variable d'env par la suite
 var env = process.env.NODE_ENV || "development";
 var config = require("../config/data.json")[env];
 
-// Constantes à exporter en variable d'env par la suite
 const BDD_HOSTURL = config.host;
+const BDD_PORT = config.port;
 const BDD_USER = config.username;
 const BDD_PASSWORD = config.password;
 const BDD_BASENAME = config.database;
 
-// SEQUELIZE
+// Création de l'objet db à exposer
+var db = {};
+
+  /**
+ * -------------- SEQUELIZE SETUP ----------------
+ */
 const sequelize = new Sequelize(BDD_BASENAME, BDD_USER, BDD_PASSWORD, {
   host: BDD_HOSTURL,
   dialect: "mysql",
 });
 
-// Création de l'objet db à exposer
-var db = {};
-
-// Chargement des modèles depuis le dossier models
-// 2 lignes par model
 const User = require('./user.model.js')(sequelize, DataTypes)
 db.user = User;
 
-// Création des associations entre les models
-// Object.keys(db).forEach(function (modelName) {
-//   if ("associate" in db[modelName]) {
-//     db[modelName].associate(db);
-//   }
-// });
+// Création des associations entre les models si besoin
 
-
-// on ajoute l'instance sequelize
-// ET la classe Sequelize pour pouvoir accéder à ses méthodes
+// on charge l'instance sequelize ET la classe pour accéder aux méthodes
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
@@ -50,6 +40,43 @@ db.sequelize
   .catch(function (err) {
     console.log(err, "Something went wrong with the Database Update!");
   });
+
+
+  /**
+ * -------------- SESSION SETUP ----------------
+ */
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+
+const sessionStockageExpiration = 1000 * 60 * 60 * 24 * 30 // 30j
+const sessionCookieExpiration = 1000 * 60 * 60 * 24 * 30 // 30j
+
+const options = {
+  host: BDD_HOSTURL,
+  port: BDD_PORT,
+  user: BDD_USER,
+  password: BDD_PASSWORD,
+  database: BDD_BASENAME,
+  // On peut gérer la durée de vie d'une session facilement
+  // les params suivant permettent de clear la db des sessions expirées
+  expiration: sessionStockageExpiration,
+  clearExpired: true,
+  checkExpirationInterval: 20000,
+};
+
+const sessionStore = new MySQLStore(options);
+
+const mSession = session({
+  key: "session_cookie_name",
+  secret: "some_session_cookie_secret",
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: sessionCookieExpiration
+  },
+})
+db.session = mSession
 
 // Expose the connection
 module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
 const passport = require("passport");
 
 const routes = require("./routes/routes.js");
@@ -11,42 +9,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+
+/**
+ * -------------- DATABASE ----------------
+ */
+const db = require("./models/database.js");
+
 /**
  * -------------- SESSION SETUP ----------------
  */
 
-const options = {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "first_bdd",
-  // On peut gérer la durée de vie d'une session facilement
-  // les params suivant permettent de clear la db des sessions expirées
-  expiration: 1000 * 15,
-  clearExpired: true,
-  checkExpirationInterval: 20000,
-};
+app.use(db.session);
 
-const sessionStore = new MySQLStore(options);
-
-app.use(
-  session({
-    key: "session_cookie_name",
-    secret: "session_cookie_secret",
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // = 1 jour
-    },
-  })
-);
-
-/**
- * -------------- MODELS ----------------
- */
-const db = require("./models/database.js");
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
@@ -62,7 +36,6 @@ app.use(passport.session());
  * -------------- ROUTES ----------------
  */
 
-// Imports all of the routes from ./routes/index.js
 app.use(routes);
 
 /**
