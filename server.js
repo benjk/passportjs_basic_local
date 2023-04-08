49359b37dc18bcb92d33846c560d86582b57b7fe
Benjam <benjamin.kielinski@hotmail.fr>
2023-04-08 03:53:59 +0200

    remove warnings

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./routes/routes.js");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
 
 
 /**
