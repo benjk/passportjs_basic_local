2b8e51dcea950f9b8703cf5460b20eccfd336bd5
Benjam <benjamin.kielinski@hotmail.fr>
2023-04-17 18:41:44 +0200

    ajout "bail", pb de plusieurs champs vides

--- src/config/validators.js
+++ src/config/validators.js
@@ -1,23 +1,29 @@
-const msg = require ('./data.json').messages
+const msg = require("./data.json").messages;
 
 const registrationSchema = {
-    password: {
-      isEmpty: { 
-        negated: true,
-        errorMessage: msg.errorPasswordEmpty,
-      },
-      isLength: {
-        options: {min: 2, max: 4},
-        errorMessage: msg.errorPasswordWeak,
-      }
+  username: {
+    isEmail: {
+      errorMessage: msg.errorEmailFormat,
     },
-    username: {
-        isEmail : {
-          errorMessage: msg.errorEmailFormat
-        }
-    }
-  }
+    isEmpty: {
+      negated: true,
+      errorMessage: msg.errorFieldEmpty,
+    },
+    bail: true // Permet de stopper la validation si cette règle n'est pas respectée
+  },
+  password: {
+    isEmpty: {
+      negated: true,
+      errorMessage: msg.errorFieldEmpty,
+      bail: true,
+    },
+    isLength: {
+      options: { min: 2, max: 4 },
+      errorMessage: msg.errorPasswordWeak,
+    },
+  },
+};
 
-  module.exports = {
-    registrationSchema
-  };
+module.exports = {
+  registrationSchema,
+};
